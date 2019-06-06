10c459356d00f901b127ff05f4b6debb1af46915
Leonardo Román Montero <leorm.24@gmail.com>
2019-06-06 11:35:41 -0600

    :)

---
+++ Proyecto/src/Model/UserType.java
@@ -0,0 +1,28 @@
+package Model;
+
+public class UserType {
+
+    private String name;
+    private String details;
+
+    public UserType(String name, String details) {
+        this.name = name;
+        this.details = details;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
