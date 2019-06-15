58bd2b9e98980376c0a4f01472cfb8ffb264e373
JoseNA <josenava@estudiantec.cr>
2019-06-14 23:37:56 -0600

    modelo -> Fabrica

---
+++ VentaCarros/src/modelo/Fabrica.java
@@ -0,0 +1,33 @@
+package modelo;
+
+public class Fabrica {
+
+    private String ID;
+    private String nombre;
+
+    public Fabrica(String ID, String nombre) {
+        this.ID = ID;
+        this.nombre = nombre;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
